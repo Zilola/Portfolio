feb4ab9e9a4cf8b8eb2725e3b4603a278cd9a709
Zilola Matasimova <zilola.matasimova@gmail.com>
2020-12-05 20:38:33 -0500

    made changes

--- dist/js/script.js
+++ dist/js/script.js
@@ -21,18 +21,18 @@ function toggleMenu() {
         navItems.forEach(item => item.classList.add('show'));
         showMenu = true;
     }
-        //Set Menu State
-     else {
-         showMenu = true;
+    //Set Menu State
+    else {
+        showMenu = true;
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
-    
+
         //Set Menu State
         showMenu = false;
-     
+
     }
 
 }
